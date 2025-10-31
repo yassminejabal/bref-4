053f86ab283d496d40168be7c5847086685f5ce2
yassmine <jabalyassmine2007@gmail.com>
2025-10-31 22:16:13 +0100

    je suis apliquer quilque notion

--- jsss.js
+++ jsss.js
@@ -1,22 +1,78 @@
-    const allEvents = document.querySelectorAll(".container >div ");
-    let bb = document.getElementsByClassName("btn");
-    let section1 = document.getElementsByClassName(".SECTION");
+// const allEvents = document.querySelectorAll(".container >div ");
+// let bb = document.getElementsByClassName("btn");
+// let section1 = document.getElementsByClassName(".SECTION");
+// let section2 = document.getElementsByClassName(".container-2");
 
 
 
-function changeDeBorder(){
-for(ev of allEvents ){
-        ev.style.border="100px solid white";
+// function changeDeBorder(Event) {
+//     for (const element of allEvents) {
+//         element.style.border = "100px solid white";
+//     }
+//     Event.currentTarget.style.border = "10px red solid";
+//     for (const element of allEvents) {
+//         element.addEventListener("click", changeDeBorder);
+//     }
+// }
+// function btn(){
+//     section1.style.display="none";
+//     section2.style.display="inline-block";
+// }
+
+const lesCarde = document.querySelectorAll(".container>div");
+const section1 = document.getElementById("SECTION");
+const section2 = document.getElementById("container-2");
+function btn() {
+    section1.style.display = "none";
+    section2.style.display = "block";
 }
-ev.currentTarget.style.border="100px solid grey";
-    console.log(ev.currentTarget);
+let date;let lieu;
+function selectioner(event){
+    // for (const element of lesCarde) {
+    //     element.addEventListener('click',()=>{
+    //     console.log(element);
+    //     element.style.border = "10px solid green";
+    //     date=element.querySelector(".test p").textContent;
+    //     console.log(date)
+    //     })
+    // }
+
+    // const carddVide = document.getElementById("cardeVide");
     
 
 }
-function btn(){
-    
-    
+    // console.log(card);
+
     
 
     
+
+//  for (const element of lesCarde) {
+//         element.addEventListner("click", selectioner);
+//     }
+// function selectioner(){
+//     for(const element of lesCarde){
+//         element.style.border="5px solid red";
+//     }
+//     for(element of lesCarde){
+//         element.currentTarget.style.
+//     }
+// }
+let lescardes=document.getElementsByClassName("container>div");
+
+function selectioner(event){
+    for(const element of lesCarde){
+        document.element.style.border="red 5px solid"
+    }
+    for(element of lesCarde){
+        element.addEventeListner("click"),()=> {
+            element.currentTarget.style.border="3px dotted red"
+        }
+        const carddVide=document.getElementsByClassName("cardeVide");
+        carddVide = document
+    }
+
+    
+
+
 }
