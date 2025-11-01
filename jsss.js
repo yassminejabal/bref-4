46ab936bee99393390d7b181887bfd3bb674902a
yassmine <jabalyassmine2007@gmail.com>
2025-11-01 17:00:07 +0100

    j'ai termainer deux foanction la premier fonction de button est la 2eme fonctio c'est de la selection et l'affichage de dans la 2eme page d'un evenment

--- jsss.js
+++ jsss.js
@@ -1,78 +1,22 @@
-// const allEvents = document.querySelectorAll(".container >div ");
-// let bb = document.getElementsByClassName("btn");
-// let section1 = document.getElementsByClassName(".SECTION");
-// let section2 = document.getElementsByClassName(".container-2");
 
-
-
-// function changeDeBorder(Event) {
-//     for (const element of allEvents) {
-//         element.style.border = "100px solid white";
-//     }
-//     Event.currentTarget.style.border = "10px red solid";
-//     for (const element of allEvents) {
-//         element.addEventListener("click", changeDeBorder);
-//     }
-// }
-// function btn(){
-//     section1.style.display="none";
-//     section2.style.display="inline-block";
-// }
-
-const lesCarde = document.querySelectorAll(".container>div");
-const section1 = document.getElementById("SECTION");
-const section2 = document.getElementById("container-2");
 function btn() {
     section1.style.display = "none";
     section2.style.display = "block";
 }
-let date;let lieu;
-function selectioner(event){
-    // for (const element of lesCarde) {
-    //     element.addEventListener('click',()=>{
-    //     console.log(element);
-    //     element.style.border = "10px solid green";
-    //     date=element.querySelector(".test p").textContent;
-    //     console.log(date)
-    //     })
-    // }
 
-    // const carddVide = document.getElementById("cardeVide");
-    
 
-}
-    // console.log(card);
-
-    
-
-    
-
-//  for (const element of lesCarde) {
-//         element.addEventListner("click", selectioner);
-//     }
-// function selectioner(){
-//     for(const element of lesCarde){
-//         element.style.border="5px solid red";
-//     }
-//     for(element of lesCarde){
-//         element.currentTarget.style.
-//     }
-// }
-let lescardes=document.getElementsByClassName("container>div");
+let lescardes = document.querySelectorAll(".container>div");
+const section1 = document.getElementById("SECTION");
+const section2 = document.getElementById("container-2");
 
 function selectioner(event){
-    for(const element of lesCarde){
-        document.element.style.border="red 5px solid"
+    for(const element of lescardes){
+        element.style.border ="solid 5px white";
     }
-    for(element of lesCarde){
-        element.addEventeListner("click"),()=> {
-            element.currentTarget.style.border="3px dotted red"
-        }
-        const carddVide=document.getElementsByClassName("cardeVide");
-        carddVide = document
+    event.currentTarget.style.border = "solid 5px red";
+    for(const element of lescardes){
+        element.addEventListener("click",selectioner);
     }
-
-    
-
-
-}
+    const carddVide = document.getElementById("cardeVide");
+    carddVide.innerHTML= event.currentTarget.outerHTML;
+    }
